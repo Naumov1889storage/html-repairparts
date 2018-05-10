e6c0f99989bedd8628352b8568383d249154e08c
nebula <maksim.naumov1889@gmail.com>
2018-05-10 11:52:38 +0500

    .crumbs

--- src/js/common.js
+++ src/js/common.js
@@ -17,4 +17,24 @@ $(function () {
        $(this).parent('.search--clear').find('input').val('')
     });
 
+    $(".crumbs__search button[type='button']").click(function () {
+        $(this).parent().find('.content').toggleClass('active');
+    });
+
+    closeIfOutside('.crumbs__search .content', '.crumbs__search .toggler--js')
+
+    function closeIfOutside(block, block2) {
+        $(document).click(function(e) {
+            var outside = $(e.target).parents().addBack();
+            if (!(outside.is(block)) && $(block).hasClass('active') && !(outside.is(block2))) {
+                $(block).removeClass('active')
+            }
+        });
+        $(document).keydown(function(e) {
+            if (e.keyCode == 27) {
+                $(block).removeClass('active')
+            }
+        });
+    };
+
 });
